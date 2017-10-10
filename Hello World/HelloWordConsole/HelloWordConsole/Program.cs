fc4b8142da6c1c2bc2e1f1a3a8d01e2c730c7f22
Marcos Furriel <mscfurriel@gmail.com>
2017-10-10 12:25:29 -0300

    Primeira aplicação em C#
    Exemplo simples para primeira aplicação utilizando C#

---
+++ Hello World/HelloWordConsole/HelloWordConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace HelloWordConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo! Hello World! ");
+            Console.ReadLine(); // espera ação do usuário para encerrar
+        }
+    }
+}
