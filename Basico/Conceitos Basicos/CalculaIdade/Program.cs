7a0dbc0220eb8610fbad8e08290ca92e4e5e09a0
Marcos Furriel <mscfurriel@gmail.com>
2017-10-14 15:40:51 -0300

    Atualização de CalculaIdade

--- Basico/Conceitos Basicos/CalculaIdade/Program.cs
+++ Basico/Conceitos Basicos/CalculaIdade/Program.cs
@@ -4,55 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/* Este código simples é apenas para aprender conceitos básicos
- * relacionados à estruturas de repetição, condição e outros.
- * 
- * O código não representa a melhor maneira se obter a idade de alguém. 
- * Também não apresenta meios para validar se a entrada do usuário está correta.
- */
-
 namespace CalculaIdade
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int diaHoje = DateTime.Now.Day;
-            int mesHoje = DateTime.Now.Month;
-            int anoHoje = DateTime.Now.Year;
 
-            int diaNasc = 0, mesNasc = 0, anoNasc = 0, idade = 0;
-
-            Console.WriteLine("Informe o dia do seu aniversario (dd/mm/aaaa) : ");
-            string dtNascimento = Console.ReadLine();
-            string[] dtNasc = dtNascimento.Split('/');
-
-            for (int i = 0; i < dtNasc.Length; i++)
-            {
-                if(i == 0)
-                {
-                    diaNasc = Convert.ToInt32(dtNasc[i]);
-                }
-                else if (i == 1)
-                {
-                    mesNasc = Convert.ToInt32(dtNasc[i]);
-                }
-                else if (i == 2)
-                {
-                    anoNasc = Convert.ToInt32(dtNasc[i]);
-                }
-            }
-
-            idade = anoHoje - anoNasc;
-
-            if((mesHoje < mesNasc) || ((mesHoje == mesNasc) && (diaHoje >= diaNasc)))
-            {
-                idade--;
-            }
-
-            Console.WriteLine("Sua idade é {0} anos", idade);
-            // Console.WriteLine("Sua idade é " + idade + " anos"); ambos funcionam. 
+            Console.WriteLine("Informe o dia em que você nasceu (dd//mm/aaaa) : ");
+            DateTime dtNasc = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine(dtNasc.ToString());
+            Console.WriteLine("Sua idade é {0:d} anos ", (DateTime.Now.Subtract(dtNasc).Days / (365)));
             Console.ReadLine();
+            //int diaHoje = DateTime.Now.Day;
+            //int mesHoje = DateTime.Now.Month;
+            //int anoHoje = DateTime.Now.Year;
+
+            //int diaNasc = 0, mesNasc = 0, anoNasc = 0, idade = 0;
+
+            //Console.WriteLine("Informe o dia do seu aniversario (dd/mm/aaaa) : ");
+            //string dtNascimento = Console.ReadLine();
+            //string[] dtNasc = dtNascimento.Split('/');
+
+            //for (int i = 0; i < dtNasc.Length; i++)
+            //{
+            //    if(i == 0)
+            //    {
+            //        diaNasc = Convert.ToInt32(dtNasc[i]);
+            //    }
+            //    else if (i == 1)
+            //    {
+            //        mesNasc = Convert.ToInt32(dtNasc[i]);
+            //    }
+            //    else if (i == 2)
+            //    {
+            //        anoNasc = Convert.ToInt32(dtNasc[i]);
+            //    }
+            //}
+
+            //idade = anoHoje - anoNasc;
+
+            //if((mesHoje < mesNasc) || ((mesHoje == mesNasc) && (diaHoje >= diaNasc)))
+            //{
+            //    idade--;
+            //}
+
+            //Console.WriteLine("Sua idade é {0} anos", idade);
+            //// Console.WriteLine("Sua idade é " + idade + " anos"); ambos funcionam. 
+            //Console.ReadLine();
 
         }
     }
